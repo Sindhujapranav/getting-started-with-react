7f6bd7f72d3987d938c610d0ada46f9ed0842796
Sindhujapranav <60981120+Sindhujapranav@users.noreply.github.com>
2020-06-25 17:48:27 +0530

    Update Agent.jsx

--- src/Router/Agent.jsx
+++ src/Router/Agent.jsx
@@ -1,37 +1,62 @@
 import React from "react";
+import table from "./table.css";
 
 class Agent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      agentlist: [],
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			agentlist: [],
+		};
+	}
 
-  componentDidMount() {
-    fetch("http://localhost:4040/agents")
-      .then((res) => res.json())
-      .then((data) => {
-        this.setState({ agentlist: data });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+	componentDidMount() {
+		fetch("http://localhost:4040/agents")
+			.then((res) => res.json())
+			.then((data) => {
+				this.setState({ agentlist: data });
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 
-  render() {
-    return (
-      <div>
-        <ul>
-          {this.state.agentlist.map((agent, index) => (
-            <li key={index}>
-              {agent.name.first} {agent.name.last} {agent.mobile} {agent.email}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className>
+				<ul>
+					{this.state.agentlist.map((agent, index) => (
+						<table key={index}>
+							<tr>
+								<td>
+									<h3>NAME</h3>
+								</td>
+								<td>
+									<h3>PHONE NO</h3>
+								</td>
+								<td>
+									<h3>EMAIL</h3>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<h4>
+										{agent.name.first}
+										{agent.name.last}
+									</h4>
+								</td>
+								<td>
+									<h4> {agent.mobile}</h4>
+								</td>
+								<td>
+									<h4>{agent.email}</h4>
+								</td>
+							</tr>
+						</table>
+					))}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default Agent;
